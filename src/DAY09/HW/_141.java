88d8ee49afd5c0748ff9a2f93aba2510ead5cbcc
dung <vhngdng@gmail.com>
2022-10-24 09:04:01 +0700

    HW DAY09

---
+++ src/DAY09/HW/_141.java
@@ -0,0 +1,79 @@
+public class _141 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(2);
+        ListNode node6 = new ListNode(1);
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node4;
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        int pos = 1;
+        if (head == null || head.next == null) {
+            return false;
+        }
+        if (head.next == head)
+            return true;
+        ListNode firstNode = head;
+        ListNode temp = head.next;
+
+        if (temp.next == null)
+            return false;
+
+        while (temp.next != null) {
+            pos++;
+            for (int i = 0; i < pos - 1; i++) {
+                if (firstNode.next == temp.next || temp.next == head.next)
+                    return true;
+                // else if (temp.next == head) {
+                //     return false;
+                // } 
+                else {
+                    firstNode = firstNode.next;
+                }
+            }
+            temp = temp.next;
+            firstNode = head;
+
+        }
+        return false;
+
+        // while (temp.next != null) {
+        // if (temp == head)
+        // return false;
+
+        // pos++;
+        // for (int i = 0; i < pos; i++) {
+        // if (firstNode == temp.next)
+        // return true;
+        // else {
+        // temp = temp.next;
+        // }
+
+        // }
+        // if (temp.next != null) {
+        // node = node.next;
+        // temp = head.next;
+        // } else
+        // return false;
+
+    }
+}
